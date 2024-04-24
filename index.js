4060fe19ddaa43bb47ab7018efd806f85a418898
ajowinga <joanwinga1@gmail.com>
2024-04-24 05:33:57 +0300

    code along

---
+++ index.js
@@ -0,0 +1,13 @@
+JS addEventListener
+// ability to listen for things that happen in the browser 
+mouse click 
+// (single and double)
+form submission 
+// upon submission eventhandler pops up a thank you or logs user in and takes them to home page etc 
+key press 
+// (keyup,keydown)
+scroll
+focus
+blur 
+onchange
+mouseenter and mouseleave
